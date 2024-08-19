126b3704300209a09c2152bc798b0a1c77f6a428
Espen Evensen <espen@evensen.io>
2024-08-19 23:46:23 +0200

    kickstart evensen.io 1.4.4 - new plugins and keymaps

---
+++ lua/evensen/plugins/bufferline-DISABLED.lua
@@ -0,0 +1,12 @@
+-- enhances buffer management by displaying buffers
+-- as a visually appealing and customizable tabline.
+return {
+  -- 'akinsho/bufferline.nvim',
+  -- dependencies = { 'nvim-tree/nvim-web-devicons' },
+  -- version = '*',
+  -- opts = {
+  --   options = {
+  --     mode = 'tabs',
+  --   },
+  -- },
+}
