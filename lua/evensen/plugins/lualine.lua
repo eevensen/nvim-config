f6de8817b078c6c5b3f121c2c9d4c6f9f4c7c6cf
Asaduzzaman Pavel <contact@iampavel.dev>
2024-09-12 01:28:04 +0600

    Feat: Improve lualine config

--- lua/evensen/plugins/lualine.lua
+++ lua/evensen/plugins/lualine.lua
@@ -56,17 +56,31 @@ return {
         theme = my_lualine_theme,
       },
       sections = {
+        lualine_a = { 'mode' },
+        lualine_b = { 'branch', 'diff' },
+        lualine_c = {
+          { 'diagnostics', symbols = { error = ' ', warn = ' ', hint = ' ', info = ' ' } },
+          { 'filetype', icon_only = true, separator = '', padding = { left = 1, right = 0 } },
+          { 'filename', path = 1, shorting_target = 40, symbols = { modified = '  ', readonly = '', unnamed = '' } },
+        },
         lualine_x = {
           {
             lazy_status.updates,
-            cond = lazy_status.has_updates,
             color = { fg = '#ff9e64' },
+            cond = lazy_status.has_updates,
           },
-          { 'encoding' },
-          { 'fileformat' },
-          { 'filetype' },
+        },
+        lualine_y = {
+          { 'progress', separator = ' ', padding = { left = 1, right = 0 } },
+          { 'location', padding = { left = 0, right = 1 } },
+        },
+        lualine_z = {
+          function()
+            return '  ' .. os.date('%R')
+          end,
         },
       },
+      extensions = { 'lazy' },
     })
   end,
 }
