882df232b8c7f9570d03d94ce189598774ff4de3
Asaduzzaman Pavel <contact@iampavel.dev>
2024-09-11 22:27:23 +0600

    Feat: Command to toggle format-on-save

--- lua/evensen/plugins/conform.lua
+++ lua/evensen/plugins/conform.lua
@@ -1,6 +1,7 @@
 -- Enhances the built-in LSP formatting capabilities by
 -- providing better diff handling and
 -- enabling range formatting for all formatters.
+
 return {
   'stevearc/conform.nvim',
   event = { 'BufReadPre', 'BufNewFile' },
@@ -22,12 +23,37 @@ return {
         python = { 'isort', 'black' },
         php = { 'phpcs', 'phpcbf' },
       },
-      format_on_save = {
-        lsp_fallback = true,
-        async = false,
-        timeout_ms = 500,
-      },
+      format_on_save = function(bufnr)
+        if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+          return
+        end
+
+        return {
+          lsp_fallback = true,
+          async = false,
+          timeout_ms = 1000,
+        }
+      end,
     })
+
+    -- Toggle autoformat on save
+    vim.api.nvim_create_user_command('FormatDisable', function(args)
+      if args.bang then
+        -- FormatDisable! will disable formatting just for this buffer
+        vim.b.disable_autoformat = true
+      else
+        vim.g.disable_autoformat = true
+      end
+    end, {
+      desc = 'Disable autoformat-on-save',
+      bang = true,
+    })
+
+    -- Re-enable autoformat on save
+    vim.api.nvim_create_user_command('FormatEnable', function()
+      vim.b.disable_autoformat = false
+      vim.g.disable_autoformat = false
+    end, { desc = 'Re-enable autoformat-on-save' })
 
     -- Format current file
     vim.keymap.set({ 'n', 'v' }, '<leader>bf', function()
